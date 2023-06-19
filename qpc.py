cfefd1819a69022605e059d82edee3820c52a1c0
net1ife <127271713+net1ife@users.noreply.github.com>
2023-06-19 15:10:29 -0400

    Update qpc.py

--- qpc.py
+++ qpc.py
@@ -1,57 +1,72 @@
+import streamlit as st
 import strawberryfields as sf
 from strawberryfields import ops
-from math import pi
 import numpy as np
+from math import pi
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+sns.set()
 
-def get_input():
-    displacement_amplitude = float(input("Enter the displacement amplitude: "))
-    displacement_phase = float(input("Enter the displacement phase: "))
-    r = float(input("Enter the squeezing parameter r: "))
-    phi = float(input("Enter the squeezing angle phi: "))
-    return displacement_amplitude, displacement_phase, r, phi
+GATES = {
+    "Displacement Operator": ops.Dgate,
+    "Squeezing Operator": ops.Sgate,
+    "Beam Splitter": ops.BSgate,
+}
 
+PARAMS = {
+    "Displacement Operator": ["abs", "phi"],
+    "Squeezing Operator": ["r", "phi"],
+    "Beam Splitter": ["theta", "phi"],
+}
+
+def simulate_circuit(num_modes, instructions):
+    prog = sf.Program(num_modes)
+    with prog.context as q:
+        for gate, params, modes in instructions:
+            GATES[gate](*params) | [q[i] for i in modes]
+    eng = sf.Engine('fock', backend_options={"cutoff_dim": 10})
+    result = eng.run(prog)
+    return result.state
+
+def visualize_state(state):
+    num_modes = state.num_modes
+    fig, ax = plt.subplots(1, num_modes, figsize=(4*num_modes, 4))
+
+    for i in range(num_modes):
+        if num_modes > 1:
+            axis = ax[i]
+        else:
+            axis = ax
+        data = state.wigner(i, np.linspace(-5,5,100), np.linspace(-5,5,100))
+        cb = axis.contourf(np.linspace(-5,5,100), np.linspace(-5,5,100), data, cmap='hot')
+        fig.colorbar(cb, ax=axis)
+        axis.set_title(f"Wigner Function (Mode {i+1})")
+
+    st.pyplot(fig)
+
+    fock_probs = state.all_fock_probs()
+    for i in range(num_modes):
+        st.bar_chart(fock_probs[:, i])  
 
 def main():
-    displacement_amplitude, displacement_phase, r, phi = get_input()
+    st.title('Quantum Photonic Circuit Simulator')
 
-    # initialize a 2-mode quantum register
-    prog = sf.Program(2)
+    num_modes = st.sidebar.slider('Number of modes', 1, 5, 2)
+    num_gates = st.sidebar.slider('Number of gates', 1, 5, 2)
 
-    with prog.context as q:
-        # Prepare displaced squeezed state in the first mode
-        ops.Dgate(displacement_amplitude, displacement_phase) | q[0]
-        ops.Sgate(r, phi) | q[0]
+    instructions = []
+    for i in range(num_gates):
+        st.sidebar.markdown(f'---\n### Gate {i+1}')
+        gate = st.sidebar.selectbox('Select a gate', list(GATES.keys()), key=f'gate{i}')
+        modes = st.sidebar.multiselect('Select modes', range(num_modes), key=f'modes{i}')
+        params = tuple(st.sidebar.slider(f'Parameter {p}', 0.0, 2.0, 1.0, key=f'param{i}{j}') for j, p in enumerate(PARAMS[gate]))
+        instructions.append((gate, params, modes))
 
-        # Apply 50-50 beam splitter
-        ops.BSgate(pi/4, 0) | (q[0], q[1])
-
-    # initialize a quantum simulator
-    eng = sf.Engine('fock', backend_options={"cutoff_dim": 5})
-
-    result = eng.run(prog)
-
-    # get the state of the quantum register
-    state = result.state
-
-    # print the Wigner function of the first mode
-    x = p = np.linspace(-5, 5, 100)
-    W = state.wigner(0, x, p)
-    plt.contourf(x, p, W)
-    plt.title("Wigner function of the first mode")
-    plt.xlabel("Position")
-    plt.ylabel("Momentum")
-    plt.show()
-
-    # print the Wigner function of the second mode
-    W = state.wigner(1, x, p)
-    plt.contourf(x, p, W)
-    plt.title("Wigner function of the second mode")
-    plt.xlabel("Position")
-    plt.ylabel("Momentum")
-    plt.show()
-
+    if st.button('Run Simulation'):
+        state = simulate_circuit(num_modes, instructions)
+        visualize_state(state)
 
 if __name__ == "__main__":
     main()
+
